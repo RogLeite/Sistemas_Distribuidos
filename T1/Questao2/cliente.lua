973306f617d6dc2b5b923b2c3622ac9ea0b8b3a6
Rodrigo Leite <rodrigo.ldasilva@hotmal.com>
2018-03-24 09:40:50 -0300

    Organiza as duas qe

---
+++ T1/Questao2/cliente.lua
@@ -0,0 +1,15 @@
+--Cliente, Fé no pai que esse programa sai
+
+local socket = require ("socket")
+for i=1,50000 do
+	local client = socket.connect('localhost',57920)
+	--if client then
+	--	print('Conectado')
+	--else
+	--	print ('offline')
+	--end
+	--print("Curr "..i)
+	client:send(i.."\n")
+	local line,status, partial = client:receive()
+	--print("Resposta "..(line or partial))
+end
---
+++ T1/Questao2/servidor.lua
@@ -0,0 +1,58 @@
+--Servidor, Fé em Deus que vai da certo
+
+--importanto socket
+local socket = require ("socket")
+
+
+--Criando socket TCP e ligando ao local host em qualquer porta.
+
+local server=assert(socket.bind("*",123456))
+-- Timeout de aguardar conexão
+server:settimeout(5)
+--descobrir qual porta o sistema operacional escolheu para nós
+local ip,porta = server:getsockname()
+
+-- print uma mensagem informando oq aconteceu
+
+print("Por favor, conecte local host na porta ", porta)
+print("Após a conexão, você tem 10 segundos para inserir uma linha")
+
+--marca inicio
+starttime = socket.gettime()
+
+--loop para esperar por clientes
+while 1 do
+	--esperando uma conexão com qualquer cliente
+	local  client, status =server:accept()
+	--se nenhum cliente conecta
+	--if status == "timeout" then
+		--print("status ==  "..status)
+		--break
+	--end
+	--print('Alguem se conectou!')
+	-- não bloquear a linha de espera do cliente
+	client:settimeout(2)
+	--recebendo dados
+	local line,err, partial = client:receive()
+	--print("err "..(err or "nil"))
+	--print("Recebeu "..(line or partial))
+	-- se não tiver erro, envia de volta ao cliente
+	if not err then 
+		client:send((line or partial).. "tai \n")
+	elseif err == "timeout" then
+		--print("err ==  "..err)
+		client:close()
+		break
+	end
+	--fechando o objeto após terminar com o cliente
+	client:close()
+end
+
+--marca fim
+endtime = socket.gettime()
+
+--calcula duração do loop
+timediff = endtime-starttime
+
+--os dois segundos subtraídos servem para compensar a espera realizada pelo servidor
+print("Fim do loop\n\t durou: "..(timediff-2).."s")
