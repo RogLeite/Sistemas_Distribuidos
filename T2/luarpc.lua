81df627ca5ae5b696289a24410ae4e45f1604c6e
Rodrigo Leite <rodrigo.ldasilva@hotmal.com>
2018-03-30 16:34:35 -0300

    Alguns conceitos foram comentados

--- T2/luarpc.lua
+++ T2/luarpc.lua
@@ -1,11 +1,44 @@
 --M (o módulo) é a tabela que será retornada ao chamar require "luarpc"
 local M = {}
-function M.createServant(object,interface)
+--tem que ver se esse local socket n deve ser declarado como M.socket ou algo do tipo (pra ter alguma persistência depois do return M)
+local socket = require "socket"
 
+
+--armazenará as corrotinas criadas por createServant(), para que possam ser enxergadas por waitIncoming()
+M.threads = {}
+
+
+function M.createServant(object,interface)
+	local co = coroutine.create(function ()
+	--código do servidor
+	
+	)
+
+
+	--[[if address is '*', the system binds to all local interfaces using the INADDR_ANY constant. If port is 0, the system automatically chooses an ephemeral port.
+	encontrei esse trecho na descrição de master:bind(), que é usado por socket.bind(), aqui:
+	http://w3.impa.br/~diego/software/luasocket/tcp.html
+	]]
+	--Acho que devemos usar 0 senão, pelo que entendi, o servidor iria se conectar a todas as portas.
+	local server=assert(socket.bind(0,123456))
+
+	--[[descobrir qual porta o sistema operacional escolheu para nós, e vamos retornar, para poderem se conectar a tal porta]]
+	local ip,porta = server:getsockname()
+	
+	--insere a corrotina criada na tabela "global"
+	table.insert(M.threads,co)
+	
 end
 
 function M.createProxy(ip, porta, interface)
-
+	--vai retornar um "objeto" que terá índices correspondentes a cada função na interface.
+	--[[
+	ex.foo = function(...) <-- ESSA É A SOLUÇÃO PRA GENTE! FUNÇÕES VARIÁDICAS!!!
+		for i,n in ipairs{...} do
+			--o tratamento dos argumentos, gerado com base na interface passada
+		end	
+	end
+	]]
 end
 
 function M.waitIncoming()
