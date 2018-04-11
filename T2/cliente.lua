91f4be2935f1ca936435f58c7c2e505a1309d000
Micaele Vieira <micaele.vieira96@gmail.com>
2018-04-11 19:02:12 -0300

    Testes

--- T2/cliente.lua
+++ T2/cliente.lua
@@ -1,6 +1,18 @@
-llocal luarpc = require "luarpc"
+local luarpc = require "luarpc"
 
-ocal p1 = luarpc.createproxy (IP, porta1, arq_interface)
-local p2 = luarpc.createproxy (IP, porta2, arq_interface)
-local r, s = p1:foo(3, 5)
-local t = p2:boo(10)
+
+print("digite ip")
+
+local ip = io.read()
+
+print ("digite porta")
+local porta = io.read()
+
+local p1 = luarpc.createProxy (ip, porta, "exinterface")
+--local p2 = luarpc.createProxy (ip, porta, "exinterface")
+--local r, s = p1:foo(3, 5)
+--local t = p2:boo(10)
+
+print(p1:foo(3,5))
+
+
--- T2/servidor.lua
+++ T2/servidor.lua
@@ -1,7 +1,7 @@
 
 local luarpc = require "luarpc"
 
----[[myobj1 = { foo = 
+myobj1 = { foo = 
              function (a, b, s)
                return a+b, "alo alo"
              end,
@@ -24,7 +24,13 @@ serv1 = luarpc.createServant (myobj1, "exinterface")
 serv2 = luarpc.createServant (myobj2, "exinterface")
 -- usa as infos retornadas em serv1 e serv2 para divulgar contato 
 -- (IP e porta) dos servidores
+ print("Conecte o cliente no IP "..serv1.ip.." e porta: ---------------------------------------- " .. serv1.porta)
+
 
 -- vai para o estado passivo esperar chamadas:
-luarpc.waitIncoming()
---]]
+
+--luarpc.waitIncoming()
+
+os.execute("sleep " .. tonumber(20))
+coroutine.resume(luarpc.threads[1])
+
